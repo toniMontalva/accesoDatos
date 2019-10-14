0ac601bef74dbeb983197d979de629c1da8c0a5a
Antonio Montalva <tonimontalvapastor@gmail.com>
2019-10-14 21:32:32 +0200

    Subo resolución no definitiva AE3

---
+++ AE3/PlaceMyBetAPI/PlaceMyBetAPI/Controllers/MercadosController.cs
@@ -0,0 +1,45 @@
+﻿using PlaceMyBetAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PlaceMyBetAPI.Controllers
+{
+    public class MercadosController : ApiController
+    {
+        // GET: api/Mercados
+        public IEnumerable<MercadoDTO> Get()
+        {
+            var repo = new MercadosRepository();
+            //List<Mercado> mercados = repo.Retrieve();
+            List<MercadoDTO> mercados = repo.RetrieveDTO();
+
+            return mercados;
+        }
+
+        // GET: api/Mercados/5
+        public List<Mercado> Get(int id)
+        {
+            return null;
+        }
+
+        // POST: api/Mercados
+        public void Post([FromBody]Mercado mercado)
+        {
+            var repo = new MercadosRepository();
+        }
+
+        // PUT: api/Mercados/5
+        public void Put(int id, [FromBody]Mercado mercado)
+        {
+        }
+
+        // DELETE: api/Mercados/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AE3/PlaceMyBetAPI/PlaceMyBetAPI/Models/MercadosRepository.cs
@@ -0,0 +1,94 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace PlaceMyBetAPI.Models
+{
+    public class MercadosRepository
+    {
+        private MySqlConnection Connect()
+        {
+            string connString = "Server=127.0.0.1;Port=3306;Database=acceso_datos;Uid=root;password=;SslMode=none";
+            MySqlConnection con = new MySqlConnection(connString);
+
+            return con;
+        }
+
+        internal List<Mercado> Retrieve()
+        {
+            MySqlConnection con = Connect();
+            MySqlCommand command = con.CreateCommand();
+            command.CommandText = "select * from mercado";
+
+            try
+            {
+                con.Open();
+                MySqlDataReader res = command.ExecuteReader();
+
+                Mercado mer = null;
+                List<Mercado> mercados = new List<Mercado>();
+                while (res.Read())
+                {
+                    Debug.WriteLine("Recuperado: " + res.GetInt32(0) + " " + res.GetInt32(1) + " " + res.GetDouble(2) + " " + res.GetDouble(3) + " " + res.GetDouble(4) + " " + res.GetDouble(5) + " " + res.GetDouble(6));
+                    mer = new Mercado(res.GetInt32(0), res.GetInt32(1), res.GetDouble(2), res.GetDouble(3), res.GetDouble(4), res.GetDouble(5), res.GetDouble(6));
+                    mercados.Add(mer);
+                }
+
+                con.Close();
+                return mercados;
+            }
+            catch (MySqlException)
+            {
+                Debug.WriteLine("Se ha producido un error de conexión.");
+                return null;
+            }
+        }
+
+        internal List<MercadoDTO> RetrieveDTO()
+        {
+            MySqlConnection con = Connect();
+            MySqlCommand command = con.CreateCommand();
+            command.CommandText = "select * from mercado";
+
+            try
+            {
+                con.Open();
+                MySqlDataReader res = command.ExecuteReader();
+
+                MercadoDTO mer = null;
+                List<MercadoDTO> mercados = new List<MercadoDTO>();
+                while (res.Read())
+                {
+                    Debug.WriteLine("Recuperado: " + res.GetInt32(0) + " " + res.GetInt32(1) + " " + res.GetDouble(2) + " " + res.GetDouble(3) + " " + res.GetDouble(4) + " " + res.GetDouble(5) + " " + res.GetDouble(6));
+                    mer = new MercadoDTO(res.GetDouble(2), res.GetDouble(3), res.GetDouble(4));
+                    mercados.Add(mer);
+                }
+
+                con.Close();
+                return mercados;
+            }
+            catch (MySqlException)
+            {
+                Debug.WriteLine("Se ha producido un error de conexión.");
+                return null;
+            }
+        }
+
+        internal void SaveMercadoNuevo(Mercado mercado)
+        {
+            MySqlConnection con = Connect();
+            MySqlCommand command = con.CreateCommand();
+            command.CommandText = "insert";
+        }
+
+        internal void UpdateMercadoExistente(int id, Mercado mercado)
+        {
+            MySqlConnection con = Connect();
+            MySqlCommand command = con.CreateCommand();
+            command.CommandText = "update";
+        }
+    }
+}
